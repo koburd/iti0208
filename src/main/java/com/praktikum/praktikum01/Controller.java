405cdf0c3378f604d3628d0418a8f052ba8feaaf
Konstantin Burdakov <koburd@ttu.ee>
2019-02-06 16:10:36 +0200

    commit prax01

---
+++ src/main/java/com/praktikum/praktikum01/Controller.java
@@ -0,0 +1,14 @@
+package com.praktikum.praktikum01;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping ("/")
+    public String index() {
+        return "Hello World, my friends!";
+
+    }
+}
---
+++ src/main/java/com/praktikum/praktikum01/Praktikum01Application.java
@@ -0,0 +1,14 @@
+package com.praktikum.praktikum01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum01Application.class, args);
+	}
+
+}
+
